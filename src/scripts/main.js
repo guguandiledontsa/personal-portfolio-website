77c3f04925f9793f09f74a032558d889240e5d60
andilegugudontsa <andiledontsa@gmail.com>
2025-06-21 16:10:31 +0200

    update :moved script tag to head suddenly js has errors cleared it

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,9 +1,1 @@
-const toggle = document.createElement('button');
-toggle.innerText = 'Toggle Dark Mode';
-document.body.append(toggle);
-
-toggle.addEventListener('click', () => {
-  document.body.classList.toggle('dark');
-});
-
 console.log("hello, world?!")
