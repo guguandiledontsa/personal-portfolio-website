0b22389a65554fc27c0ca487e6f70023c73846ad
guguandiledontsa <andiledontsa@gmail.com>
2025-09-13 02:50:21 +0200

    update utils.js

--- src/tests/utils.js
+++
@@ -1,7 +0,0 @@
-  <script type="module">
-    // Load Mocha first
-    import 'https://cdn.jsdelivr.net/npm/mocha@10.5.2/mocha.js';
-
-    // Then load your test file
-    import './tests/test-styles.js';
-  </script>
